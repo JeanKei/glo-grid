3cb66be7d3bcd550b34dbbaba3fda292a1009f50
Jean Kei <jeankei@yandex.ru>
2021-11-06 02:50:57 +0900

    Разметил грид на шапку

---
+++ build/js/main.js
@@ -0,0 +1,13 @@
+
+// =======================БУРГЕР===========================
+// const menuButton = document.querySelector('.icon-menu');
+// const menu = document.querySelector('.menu__body');
+// menuButton.addEventListener("click", (event) => {
+//   menu.classList.toggle("_active");
+// });
+
+// const menuButton2 = document.querySelector('.icon-menu');
+// const menu2 = document.querySelector('.icon-menu');
+// menuButton2.addEventListener("click", (event) => {
+//   menu2.classList.toggle("_active");
+// });
